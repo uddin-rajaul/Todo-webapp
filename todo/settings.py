79fe07ff6bfe542aa7c091d67a525b957e7c07a9
Rajaul <uddin.rajaul1@gmail.com>
2023-08-05 19:50:32 +0545

    small bug

--- todo/settings.py
+++ todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower()=="true"
 
-ALLOWED_HOSTS = os.environ.get("ALOWED HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALOWED_HOSTS").split(" ")
 
 
 
