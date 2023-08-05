e71f8ab3553527b8edd0e71449a7bc165e4b3f39
Rajaul <uddin.rajaul1@gmail.com>
2023-08-05 21:35:33 +0545

    mimetypes

--- todo/settings.py
+++ todo/settings.py
@@ -12,10 +12,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import dj_database_url 
 from pathlib import Path
 import os
-
+import mimetypes
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
+mimetypes.add_type("text/css", ".css", True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
